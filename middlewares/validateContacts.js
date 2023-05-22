9be572ee1c4985d883b787ed4024f7ccd9d3f536
Andrew Steshenko <steshweb@gmail.com>
2023-05-22 12:13:29 +0300

    fix error message

--- middlewares/validateContacts.js
+++ middlewares/validateContacts.js
@@ -2,9 +2,15 @@ const {HttpError} = require('../helpers')
 
 const validateContacts = schema => {
   const func = (req, res, next) => {
+    const bodyLength = Object.keys(req.body).length
+      if(!bodyLength) {
+      throw HttpError(400, "missing fields")
+    }
+
     const {error} = schema.validate(req.body)
     if(error) {
-      throw HttpError(400, 'missing required field')
+      const fieldName = error.details[0].context.key
+      throw HttpError(400, 'missing required ' + fieldName + ' field')
     }
     next()
   }
