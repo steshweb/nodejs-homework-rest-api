2102620f405eb7b7b2bb303f3b0795382fef4962
Andrew Steshenko <steshweb@gmail.com>
2023-06-04 14:24:41 +0300

    finish hw-06

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./HttpError')
 const ctrlWrapper = require('./ctrlWrapper')
 const avatarOptimazation = require('./avatarOptimization')
+const sendEmail = require('./sendEmail')
 
 module.exports = {
   HttpError,
   ctrlWrapper,
-  avatarOptimazation
+  avatarOptimazation,
+  sendEmail
 }
